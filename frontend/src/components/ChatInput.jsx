d2faff976db993285ab3db9df7340ff6f125adac
DurgePratik <iitianpratik2022@gmail.com>
2026-06-15 14:18:46 +0530

    Fix

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -219,7 +219,7 @@ function ChatInput({
                 >
                     {
                         loading
-                            ? "⌛"
+                            ? "■"
                             : "↑"
                     }
                 </button>
